61f599c04d2f87ff6ed43b3fd94fa3f7a366260e
WKapenda <wkapenda@gmail.com>
2021-04-25 09:06:32 +0200

    Linked client and contact, need to count and display # of c/c

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -20,6 +20,16 @@ namespace BusinessDirectoryApp.Data
             modelBuilder.Entity<ClientContact>()
                        .HasKey(c => new { c.ClientID, c.ContactID });
 
+            // Create one to many relationship between ClientContact & Client
+            modelBuilder.Entity<ClientContact>()
+            .HasOne<ClientModel>(c => c.Client)
+            .WithMany(p => p.ClientContact);
+
+            // Create one to many relationship between ClientContact & Contact
+            modelBuilder.Entity<ClientContact>()
+            .HasOne<ContactModel>(c => c.Contact)
+            .WithMany(p => p.ClientContact);
+
             base.OnModelCreating(modelBuilder);
         }
 
